a33d19ac227f72d75a3cbfac1a874659efa080f1
I3NoCraK <kelvinemanueltrx@gmail.com>
2025-08-25 21:24:43 -0300

    Update Robux.lua

--- Robux.lua
+++ Robux.lua
@@ -1,4 +1,13 @@
 return {
-    I3_NoPrime = true,
-    roblox = true
+    Permissions = {
+        I3_NoPrime = 3,
+        i3ueueei = 2,
+        OutroPlayer = 1
+    },
+
+    Tiers = {
+        [1] = {"DoubleJump"},
+        [2] = {"DoubleJump", "KunaiKajin", "Counter"},
+        [3] = {"DoubleJump", "KunaiKajin", "Susanno", "Counter", "Colateral", "Corte"}
+    }
 }
