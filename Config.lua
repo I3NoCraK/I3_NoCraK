5d7a60d260f7ce65f3bb70a79ac7a4e4b655e125
I3NoCraK <kelvinemanueltrx@gmail.com>
2025-08-25 21:26:35 -0300

    Update and rename Robux.lua to Config.lua

---
+++ Config.lua
@@ -0,0 +1,12 @@
+{
+    "Permissions": {
+        "I3_NoPrime": 3,
+        "i3ueueei": 2,
+        "OutroPlayer": 1
+    },
+    "Tiers": {
+        "1": ["DoubleJump"],
+        "2": ["DoubleJump", "KunaiKajin", "Counter"],
+        "3": ["DoubleJump", "KunaiKajin", "Susanno", "Counter", "Colateral", "Corte"]
+    }
+}
