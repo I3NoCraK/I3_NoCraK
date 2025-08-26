3366f8ea5e13c1b3d43952a3668d71fcacb3818e
I3NoCraK <kelvinemanueltrx@gmail.com>
2025-08-25 21:34:39 -0300

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,12 +1,11 @@
 {
-    "Permissions": {
-        "I3_NoPrime": 3,
-        "i3ueueei": 2,
-        "OutroPlayer": 1
-    },
-    "Tiers": {
-        "1": ["DoubleJump"],
-        "2": ["DoubleJump", "KunaiKajin", "Counter"],
-        "3": ["DoubleJump", "KunaiKajin", "Susanno", "Counter", "Colateral", "Corte"]
-    }
+  "Users": {
+    "1663412321": { "Tier": 3 },
+    "i3ueueei": { "Tier": 2 }
+  },
+  "Tiers": {
+    "1": { "Items": ["DoubleJump"] },
+    "2": { "Items": ["DoubleJump","KunaiKajin","Counter"] },
+    "3": { "Items": ["DoubleJump","KunaiKajin","Susanno","Counter","Colateral","Corte"] }
+  }
 }
